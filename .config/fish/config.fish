94a93c0a8cf4747126e50e6e2434f042a09ccc6d
welsny <zengw@mssm.org>
2016-08-04 00:14:56 -0400

    fix config.fish ... syntax error smh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@
 alias vim nvim
 
 switch (uname)
-    case Darwin:
+    case Darwin
         alias vlc "open -a /Applications/VLC.app/"
         alias word "open -a /Applications/Microsoft\ Word.app/"
 
@@ -16,13 +16,13 @@ end
 
 switch (uname)
     # OSX Mouse/Trackpad Acceleration:
-    case Darwin:
+    case Darwin
         defaults write .GlobalPreferences com.apple.mouse.scaling -1
         defaults write -g com.apple.trackpad.scaling -float 5.0
         defaults write -g com.apple.mouse.scaling -float 5.0
   
     # Linux Button Mapping:
-    case Linux:
+    case Linux
         echo 'xinput set-button-map $(xinput | grep "Evoluent" | awk {"print substr(7,4,2)"}) 1 3 3 4 5 6 7 9 2 8 11 12 13 14' | sh
         setxkbmap -option ctrl:nocaps
 end
