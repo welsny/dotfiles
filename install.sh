b58af1bacfc67db2f5ca802abcd38e28d454e449
welsny <zengw@mssm.org>
2016-08-03 23:33:10 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,19 @@ if [ $(uname -s) = 'Darwin' ]; then
     [ -z "$(which brew)" ] &&
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+    brew tap caskroom/cask
+
     brew cask install \
         google-chrome \
-        iterm2
+        iterm2 \
+        flux \
+        vlc
 
     brew install \
         fish \
         neovim \
         git \
+        hub \
         diff-so-fancy \
         python
 
@@ -21,9 +26,11 @@ if [ $(uname -s) = 'Darwin' ]; then
         virtualfish
 fi
 
-mkdir -p ~/.config/nvim/autoload
-curl --insecure -fLo ~/.config/nvim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
+chsh -s `which fish`
 
+mkdir -p ~/.config/nvim/autoload
+curl --insecure -fLo ~/.config/nvim/autoload/plug.vim \
+    https://raw.github.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
 python ~/.config/nvim/plugged/YouCompleteMe/install.py
